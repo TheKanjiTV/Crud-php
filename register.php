8836935e4a94510081fd7f290c8b61083a3cf028
thekanjitv <kenjiyonaha11@gmail.com>
2026-04-17 20:52:57 +0800

    New

--- register.php
+++ register.php
@@ -40,7 +40,7 @@ if (isset($_SESSION['user_id'])) {
             </div>
         </form>
         <p class="text-center text-gray-500 text-xs">
-            &copy;2024 Your Company. All rights reserved.
+            &copy;2026 Kenji Yonaha. All rights reserved.
         </p>
     </div>
 
